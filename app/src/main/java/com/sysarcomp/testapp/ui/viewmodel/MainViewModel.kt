104c8b7cd301a6272cd00bb77adb1d62d248a86e
sysar <patricionguirre@gmail.com>
2024-09-09 14:24:34 -0300

    init

--- app/src/main/java/com/sysarcomp/testapp/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/sysarcomp/testapp/ui/viewmodel/MainViewModel.kt
@@ -1,6 +1,31 @@
 package com.sysarcomp.testapp.ui.viewmodel
 
-class MainViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sysarcomp.testapp.data.model.MovieList
+import com.sysarcomp.testapp.data.repository.MovieRepository
+import kotlinx.coroutines.launch
 
+class MainViewModel : ViewModel() {
 
+    private val movieRepository = MovieRepository()
+    private val _movies = MutableLiveData<List<MovieList>>()
+    val user: LiveData<List<MovieList>> get() = _movies
+
+    fun fetchMovies() {
+        viewModelScope.launch {
+
+            try {
+                val fetchedMovies: List<MovieList> = movieRepository.getTrendingWeeklyMovies()
+                _movies.value = fetchedMovies
+
+            } catch (e: Exception) {
+
+                // Manejar el error
+                _movies.value = emptyList()
+            }
+        }
+    }
 }
